a6b626b2c65480174842335bd095d6f7105bfa44
tomlord <aa123593465@gmail.com>
2025-05-13 23:00:16 +0800

    chore: modify title name

--- src/routes/photography/+page.svelte
+++ src/routes/photography/+page.svelte
@@ -15,7 +15,7 @@
 	<!-- A large heading for the page title or your name -->
 	<h1 
 		class="page-title">
-		Photography
+		tom.photography
 	</h1>
 
 	<main in:fly={{ y: 100, duration: 1000, delay: 600 }} 
--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -8,7 +8,7 @@
 	<!-- A large heading for the page title or your name -->
 	<h1 
 		class="page-title">
-		Project
+		tom.project
 	</h1>
 
 	<main in:fly={{ y: 100, duration: 1000, delay: 600 }} 
