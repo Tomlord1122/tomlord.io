c0215fd2814741cd174f25a68ad9331b2a538df9
tomlord <aa123593465@gmail.com>
2025-05-16 22:34:33 +0800

    fix: correct alt text for photo 4 and add photo 5 to the photography page

--- src/routes/photography/+page.svelte
+++ src/routes/photography/+page.svelte
@@ -6,7 +6,8 @@
 		{ src: '/photo/1.png', alt: 'Photo 1' },
 		{ src: '/photo/2.png', alt: 'Photo 2' },
 		{ src: '/photo/3.png', alt: 'Photo 3' },
-		{ src: '/photo/4.jpeg', alt: 'Photo 3' },
+		{ src: '/photo/4.jpeg', alt: 'Photo 4' },
+		{ src: '/photo/5.jpeg', alt: 'Photo 5' },
 	];
 </script>
 
