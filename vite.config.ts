787c6ab025e848c2baea2a321fc52e2934a6adc5
Tom Liu (LIU HSIU-CHI) <tom_liu@trendmicro.com>
2025-12-13 08:03:11 +0800

    chore: remvoe image-tool for faster start up
    Signed-off-by: Tom Liu (LIU HSIU-CHI) <tom_liu@trendmicro.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit(),
-		imagetools({
-			defaultDirectives: new URLSearchParams({
-				format: 'webp'
-			})
-		})
-	],
+	plugins: [tailwindcss(), sveltekit()],
 	optimizeDeps: {
 		exclude: ['sharp']
 	},
